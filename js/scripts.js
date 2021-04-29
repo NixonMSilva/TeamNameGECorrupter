5cb8dbf67020ee4e82d35ae68a2d5996e7392d3a
Nixon Silva <nixonmoreira42@gmail.com>
2021-04-29 11:23:36 -0300

    Version 1.4 - Dynamic Name Cropping
    Added a new method of generating random names by randomly cropping the team names rather than using a pre-set array of crops. The generator is now more random but also generates more non-sensical results.

--- js/scripts.js
+++ js/scripts.js
@@ -1,138 +1,170 @@
 const maxNames = 2000;
 
-const noMethods = 9;
+const noMethods = 3;
 
 var allowNonAmericanClubs = true;
 
-// Club names on the left
+var name_0 = "", name_1 = ""
 
-const clubNamesLeft = ["Fla", "Flu", "São ", "San", "Pal", "Cor", "Athleti", "Atléti", "Cru", "Bota", 
-	"Vas", "Gre", "Chape", "Boca ", "River ", "San ", "Peña", "Tigre", "Bangu", "Forta", "Cea", "Vit", "Bah", 
-	"Figuei", "Ava", "Crici", "Cuia", "Corin", "Náuti", "Santa", "Spor", "Améri", "Coriti", "Ponte", "Guara", "Grêmi",
-	"Bragan", "Confi", "Cruzei", "Flamen", "Flumi", "Vasco", "Interna", "Inter", "Vitó", "Bahi", "Ponte Pre",
-	"São Pau"]
+const vowels = ['a', 'e', ,'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U',
+	"á", "Á", "é", "É", "í", "Í", "ó", "Ó", "ú", "Ú", "â", "Â",
+	"ã", "Ã", "ê", "Ê", "ô", "Ô"]
 
-// Club names on the left (Non-American)
+// Club names (Brazil)
 
-const clubNamesLeft_NAMER = ["Real ", "Barce", "Manchester ", "Juve", "Lazi", "Arse", "Porto", "Benfi", "Bayern ",
-	"Liver", "Chel", "Totten", "Mazem", "Raja ", "Ata", "Paris Saint-", "Olympique ", "Roma", "Sportin", "Shakt",
-	"Galata", "Fener", "Dínamo ", "Schalke", "Borussia ", "Mil", "Milan"]
+const clubNames = ["Flamengo", "Fluminense", "Vasco", "São Paulo", "Santos", "Palmeiras", "Corinthians",
+	"Athlético", "Atlético", "Cruzeiro", "Botafogo", "Grêmio", "Internacional", "Chapecoense", "Ponte Preta",
+	"Guarani", "Fortaleza", "Ceará", "Bahia", "Vitória", "Figueirense", "Avaí", "Criciúma", "Juventude",
+	"Brasil de Pelotas", "Coritiba", "Paraná", "Confiança", "Bragantino", "América", "América-RN", "Goiás",
+	"Atlético-GO", "Vila Nova", "Cuiabá", "São Caetano", "Santa Cruz", "Náutico", "Sport Recife", "Paysandu",
+	"Remo", "Atlético Mineiro", "Athétlico Paranaense", "CSA", "CRB", "Rominthians"];
 
-// Club names on the right, starting with consonants
+// Club names (Foreign)
 
-const clubNamesRightConsonant = ["mengo", "minense", "tos", "meiras", "zeiro", "fogo",  "coense", "rol", "rá", "leza", 
-	"nacional", "mio", "rense", "rani", "vaí", "cional", "lético", "rinthians", "ntino", "taleza"]
-
-// Club names on the right, starting with vowels
-
-const clubNamesRightVowel = ["inthians", "entude", "ória", "úma", "asco", "afogo", "itiba", "áutico", "abá", 
-	"ia", "ória", "antino", "onfiança", "eará"]
-
-// Club names on the right, starting with consonants (Non-American)
-
-const clubNamesRightConsonant_NAMER = ["lona", "nfica", "pool", "sea", "ham", "zembe", "nazionale", "lanta", "saray",
-	"bahçe", "ktas", "noord"]
-
-// Club names on the right, starting with vowels (Non-American)
-
-const clubNamesRightVowel_NAMER = ["enham", "enfica", "iktas", "oord", "-Germain"]
-
-// Composite club names
-
-const clubNamesComposite = [" Juniors", " Plate", " Lorenzo", " Paulo", " Mineiro", " Paranaense", 
-	" do Rio", " de Natal", " Goianiense", " Paraibano", " de Alagoinhas", "-MG", "-RN", "-PR", 
-	"-RJ", "-PB", "-GO", " Cruz", " Del Valle"]
-
-// Composite club names (Non-American)
-
-const clubNamesComposite_NAMER = [" United", " Madrid", " City", " de Munique", " Casablanca", " Dortmund", " Zagreb",
-	" Zvezda", " de Madrid"]
+const clubNamesForeign = ["Peñarol", "Nacional", "River Plate", "Boca Juniors", "Milan", "Internazionale",
+	"Juventus", "Roma", "Lazio", "Napoli", "Atalanta", "Paris Saint-Germain", "Olympique de Lyon", "Arsenal", 
+	"Chelsea", "Tottenham", "Liverpool", "Manchester United", "Manchester City", "Everton", "Barcelona", 
+	"Real Madrid", "Atlético de Madrid", "Sevilla", "Bayern de Munique", "Borussia Dortmund", "Schalke", "Porto", 
+	"Befinca", "Sporting", "Braga", "Dínamo Kiev", "Dínamo Zagreb", "Cverna Zvezda", "Olympiakos", "Panathinaikos", 
+	"Besiktas", "Fenerbahçe", "Galatasaray", "CSKA Moscou", "Zenit São Petersburgo", "Lokomotiv Moscou", "Ajax", "Feyenoord", 
+	"PSV Eindhoven", "Club Brugge", "Young Boys", "Legia Varsóvia", "Shaktar Donetsk", "Mazembe", "Raja Casablanca", "Al-Ahly",
+	"Al-Hilal", "Kashima Antlers", "Gamba Osaka", "Yokohama F-Marinos", "Ulsan Hyundai", "Shanghai SIPG", "Guangzhou Evergrande",
+	"Seattle Sounders", "Atalanta United", "Toronto", "Los Angeles Galaxy", "Tigres", "América", "Necaxa", "Monterrey",
+	"Independiente Del Valle", "EMELEC", "Colo-Colo"]
 
 // Generic corruptions (can be applied on any side)
 
 const corruptionsGeneric = ["Merda", "Cocô", "Lixo", "Bosta", "Putrefa", "Desgraça", "Caralho", "Porra", "Verme",
-	"Discórdia", "Bumbum", "Otário", "Debilóide", "Rouba", "VAR", "Caga", "Mijo", "Xixi", "Urina", "Fezes", "Mia Khalifa",
-	"Gorfo", "Cachaça", "Cu", "Corno", "Máfia", "Apito", "Crime", "Assalto", "Pau"]
+	"Discórdia", "Bumbum", "Otário", "Debilóide", "Rouba", "Caga", "Mijo", "Xixi", "Urina", "Fezes", "Mia Khalifa",
+	"Gorfo", "Cachaça", "Cu", "Corno", "Máfia", "Apito", "Crime", "Assalto", "Pau", "Falência", "Mídia", "Órfão", "Bilau",
+	"Piupiu", "Pênis", "Vagina", "Bunda", "Chupada", "Boquete", "Fio-terra", "Gozo", "Esperma", "Pircoa", "Mamador", "Lambida",
+	"Arrombada", "Diarreia", "Leprosa", "Aidéica", "Bumbumguloso", "Puta", "Satanás"]
 
-// Corruptions at the start of the name (prefix)
+// Acronym corruptions (does not lower case the first character)
 
-const corruptionsLeft = ["VAR", "Putref", "Desgraç", "Caralh", "Mij", "Cag", "Roub", "Verm", "Porr", "Bost", "Cocô", "Merd", 
-	"Apit", "Crim", "Assalt"]
-
-// Corruptions at the end of the name (suffix)
-
-const corruptionsRight = ["bumbumguloso", "falência", "puta", "mídia", "órfão", "bilau", "piupiu", "pênis", "vagina",
-	"bunda", "chupada", "boquete", "fio-terra", "gozo", "esperma", "piroca", "mamador", "lambida", "arrombada",
-	"diarreia", "leprosa", "aidética"]
+const corruptionsAcronyms = ["VAR", "PCC", "AIDS", "HIV", "COVID", "FDP", ""]
 
 const generateName = () => {
 
-	// Picks two seeds then choose a method of merging
-	var corruptedName = "", seed_0 = generateRandomNumber(), seed_1 = generateRandomNumber();
-	var  choosenMethodNo = chooseMethod();
-
-	// Check if non-american clubs are allowed through the radio buttons
-	if (!allowNonAmericanClubs)
-		choosenMethodNo %= 4;
+	// Dynamic pool of team names and corruptions
+	var namePool = [], corruptionPool = [];
 	
-	// Produce the result
-	switch (choosenMethodNo)
+	// 1 - Left: Team, Right: Corruption | 2 - Left: Corruption, Right: Team 
+	var pickOrder = generateRandomNumber(2);
+
+	// 1..10 - Acronym | 11..100 - Generic
+	var pickCorruptionType = generateRandomNumber(100);
+
+	// Check to see if the corruption can get lowecased (disabled for acronyms)
+	var canChangeToLowerCase = true;
+
+	// Check whether to add or not foreign club names to the generator pool
+	if (allowNonAmericanClubs)
 	{
-		case 0:	// clubNamesLeft + corruptionsGeneric
-			corruptedName = getRandomElement(clubNamesLeft, seed_0) + getRandomElement(corruptionsGeneric, seed_1);
-		break;
-		case 1: // clubNamesLeft + corruptionsRight
-			corruptedName = getRandomElement(clubNamesLeft, seed_0) + getRandomElement(corruptionsRight, seed_1);
-		break;
-		case 2: // corruptionsGeneric + clubNamesRightConsonant
-			corruptedName = getRandomElement(corruptionsGeneric, seed_0) + getRandomElement(clubNamesRightConsonant, seed_1);
-		break;
-		case 3: // corruptionsLeft + clubNamesRightVowel
-			corruptedName = getRandomElement(corruptionsLeft, seed_0) + getRandomElement(clubNamesRightVowel, seed_1);
-		break;
-		case 4: // corruptionsGeneric + clubNamesComposite
-			corruptedName = getRandomElement(corruptionsGeneric, seed_0) + getRandomElement(clubNamesComposite, seed_1);
-		break;
+		namePool = clubNames;
+		namePool.concat(clubNamesForeign);
+	}
+	else
+		namePool = clubNames;
 
-		// Cases with Non-America clubs
+	// Check which type of corruption to pick
+	if (pickCorruptionType <= 10)
+	{
+		corruptionPool = corruptionsAcronyms;
+		canChangeToLowerCase = false;
+	}
+	else
+		corruptionPool = corruptionsGeneric;
 
-		case 5: // clubNamesLeft_NAMER + corruptionsGeneric
-			corruptedName = getRandomElement(clubNamesLeft_NAMER, seed_0) + getRandomElement(corruptionsGeneric, seed_1);
+	switch (pickOrder)
+	{
+		case 1:	// Left: Team name | Right: Corruption
+			name_0 = cropTeamName(getTeamName(namePool), true);
+			name_1 = getCorruptionName(corruptionPool);
+			if (canChangeToLowerCase)
+				name_1 = changeToLowerCase(name_1);
 		break;
-		case 6: // clubNamesLeft_NAMER + corruptionsRight
-			corruptedName = getRandomElement(clubNamesLeft_NAMER, seed_0) + getRandomElement(corruptionsRight, seed_1);
-		break;
-		case 7: // corruptionsGeneric + clubNamesRightConsonant_NAMER
-		corruptedName = getRandomElement(corruptionsGeneric, seed_0) + getRandomElement(clubNamesRightConsonant_NAMER, seed_1);
-		break;
-		case 8: // corruptionsLeft + clubNamesRightVowel_NAMER
-			corruptedName = getRandomElement(corruptionsLeft, seed_0) + getRandomElement(clubNamesRightVowel_NAMER, seed_1);
-		break;
-		case 9: // corruptionsGeneric + clubNamesComposite
-			corruptedName = getRandomElement(corruptionsGeneric, seed_0) + getRandomElement(clubNamesComposite_NAMER, seed_1);
+		case 2:	// Left: Corruption | Right: Team name
+			name_0 = getCorruptionName(corruptionPool);
+			name_1 = changeToLowerCase(cropTeamName(getTeamName(namePool), false));
 		break;
 	}
+
+	// Check if the first string last letter can be cropped or not
+	if (canParseVowelConsonant(name_0, name_1)) {
+		name_0 = parseVowelConsonant(name_0);
+	}
+
+	// Merge the two parts of the name
+	corruptedName = name_0 + name_1;
 
 	// Edit the values of the readonly input to show the result
 	document.getElementById("result").value = corruptedName;
 };
 
-const generateRandomNumber = () => {
-	return Math.floor((Math.random() * maxNames)) + 1;
-};
+const generateRandomNumber = (maxNo) => Math.floor((Math.random() * maxNo)) + 1;
 
-const getRandomElement = (arr, number) => {
-	return arr[number % arr.length];
-};
+const getRandomElement = (arr, number) => arr[number % arr.length];
 
-const chooseMethod = () => {
-	return Math.floor((Math.random() * (noMethods + 1)));
+const chooseMethod = () => Math.floor((Math.random() * (noMethods + 1)));
+
+const changeAllowEuropeanClubs = () => allowNonAmericanClubs = true;
+
+const changeDisallowEuropeanClubs = () => allowNonAmericanClubs = false;
+
+const canParseVowelConsonant = (stringA, stringB) => {
+	// Checks if the last letter of the first string is a vowel
+	if (vowels.includes(stringA[stringA.length - 1]))
+	{
+		// Checks if the first letter of the second string is also vowel
+		// if so, trims one letter of the first string
+		if (vowels.includes(stringB[0]))		
+			return true;
+	}
+	else 
+	{
+		// Trim if both are consonants as well
+		if (!vowels.includes(stringB[0])) 
+			return true;
+	}
+	return false;
 }
 
-const changeAllowEuropeanClubs = () => {
-	allowNonAmericanClubs = true;
+const parseVowelConsonant = (string, isFirst) => {
+	// Failsafe to not trim small strings (<= 3 length)
+	if (string.length <= 3)
+		return string;
+
+	if (isFirst)
+	{
+		return string.slice(1);
+	}
+	else
+	{
+		return string.slice(0, string.length - 1);
+	}
 }
 
-const changeDisallowEuropeanClubs = () => {
-	allowNonAmericanClubs = false;
+const cropTeamName = (string, isLeft) => {
+	// Allow at least 3 characters in the team name
+	cropPoint = 2 + generateRandomNumber(string.length - 3);
+
+	// isLeft checks whether to trim from right to left (true)
+	// or left to right (false)
+	if (isLeft)
+		return string.slice(0, cropPoint - 1);
+	else
+		return string.slice(cropPoint);
+}
+
+const changeToLowerCase = (string) => {
+	return string.charAt(0).toLowerCase() + string.slice(1);
+}
+
+const getTeamName = (arr) => {
+	return getRandomElement(arr, generateRandomNumber(maxNames));
+}
+
+const getCorruptionName = (arr) => {
+	return getTeamName(arr);
 }
