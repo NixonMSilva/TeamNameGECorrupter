c6fd870293b3772b82a10d0c1c0b57d389181723
Nixon Silva <nixonmoreira42@gmail.com>
2021-04-28 22:19:59 -0300

    New repo update

--- js/scripts.js
+++ js/scripts.js
@@ -2,30 +2,34 @@ const maxNames = 2000;
 
 const noMethods = 4;
 
-const clubNamesLeft = ["Fla", "Flu", "São", "San", "Pal", "Cor", "Athleti", "Atléti", "Cru", "Bota", 
+const clubNamesLeft = ["Fla", "Flu", "São ", "San", "Pal", "Cor", "Athleti", "Atléti", "Cru", "Bota", 
 	"Vas", "Real ", "Barce", "Manchester ", "Juve", "Inter", "Gre", "Lazi", "Porto", "Benfi", "Bayern ",
 	"Chape", "Boca ", "River ", "San ", "Peña", "Liver", "Chel", "Totten", "Mazem", "Raja ", "Tigre",
 	"Bangu", "Forta", "Cea", "Vit", "Bah", "Ata", "Figuei", "Ava", "Crici", "Cuia", "Corin", "Náuti",
-	"Santa", "Spor", "Améri", "Coriti", "Ponte ", "Guara"]
+	"Santa", "Spor", "Améri", "Coriti", "Ponte", "Guara", "Roma", "Sportin", "Shakt", "Galata", "Fener",
+	"Aja", "Feyer", "Dínamo ", "Schalke", "Grêmi"]
 
-const clubNamesRightConsonant = ["mengo", "minense", "tos", "meiras", "zeiro", "fogo", "lona", "ca", 
-	"coense", "rol", "pool", "sea", "ham", "be", "entude", "rá", "leza", "ia", "ória", "nacional", "mio",
-	"lanta", "rense", "bá", "ni"]
+const clubNamesRightConsonant = ["mengo", "minense", "tos", "meiras", "zeiro", "fogo", "lona", "nfica", 
+	"coense", "rol", "pool", "sea", "ham", "zembe", "rá", "leza", "nacional", "mio",
+	"lanta", "rense", "rani", "saray", "bahçe", "ktas", "noord", "vaí"]
 
-const clubNamesRightVowel = ["inthians", "entude", "ória", "úma", "í", "asco", "afogo", "itiba", "áutico"]
+const clubNamesRightVowel = ["inthians", "entude", "ória", "úma", "asco", "afogo", "itiba", "áutico", "abá", 
+	"iktas", "oord", "ia", "ória", "enham", "enfica"]
 
 const clubNamesComposite = [" United", " Madrid", " City", " de Munique", " Casablanca", " Juniors", " Plate",
 	" Lorenzo", " Paulo", " Mineiro", " Paranaense", " do Rio", " de Natal", " Goianiense", " Dortmund", " Zagreb", 
-	" Zvezda", " de Madrid", " Paraibano", " de Alagoinhas", "-MG", "-RN", "-PR", "-RJ", "-PB", "-GO", " Cruz"]
+	" Zvezda", " de Madrid", " Paraibano", " de Alagoinhas", "-MG", "-RN", "-PR", "-RJ", "-PB", "-GO", " Cruz",
+	" Del Valle"]
 
 const corruptionsGeneric = ["Merda", "Cocô", "Lixo", "Bosta", "Putrefa", "Desgraça", "Caralho", "Porra", "Verme",
 	"Discórdia", "Bumbum", "Otário", "Debilóide", "Rouba", "VAR", "Caga", "Mijo", "Xixi", "Urina", "Fezes", "Mia Khalifa",
-	"Gorfo", "Cachaça", "Cu"]
+	"Gorfo", "Cachaça", "Cu", "Corno", "Máfia", "Apito", "Crime", "Assalto"]
 
-const corruptionsLeft = ["VAR", "Putref", "Desgraç", "Caralh", "Mij", "Cag", "Roub", "Verm", "Porr", "Bost", "Coc", "Merd" ]
+const corruptionsLeft = ["VAR", "Putref", "Desgraç", "Caralh", "Mij", "Cag", "Roub", "Verm", "Porr", "Bost", "Coc", "Merd", 
+	"Apit", "Crim", "Assalt"]
 
 const corruptionsRight = ["bumbumguloso", "falência", "puta", "mídia", "órfão", "bilau", "piupiu", "pênis", "vagina",
-	"bunda", "chupada", "boquete", "fio-terra"]
+	"bunda", "chupada", "boquete", "fio-terra", "gozo", "esperma"]
 
 const generateName = () => {
 	var corruptedName = "", seed_0 = generateRandomNumber(), seed_1 = generateRandomNumber();
