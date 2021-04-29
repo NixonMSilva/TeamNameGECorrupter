6ddb12a0f8a60a6fc621dc8774180e69bc79e6e7
Nixon Silva <nixonmoreira42@gmail.com>
2021-04-28 21:10:19 -0300

    Version 1.0 Release
    It generates corrupted team names, still needs improvement on the ortography and changing characters to their proper case depending on the situation.

---
+++ js/scripts.js
@@ -0,0 +1,60 @@
+const maxNames = 2000;
+
+const noMethods = 4;
+
+const clubNamesLeft = ["Fla", "Flu", "São", "San", "Pal", "Cor", "Athleti", "Atléti", "Cru", "Bota", 
+	"Vas", "Real ", "Barce", "Manchester ", "Juve", "Inter", "Gre", "Lazi", "Porto", "Benfi", "Bayern ",
+	"Chape", "Boca ", "River ", "San ", "Peña", "Liver", "Chel", "Totten", "Mazem", "Raja ", "Tigre",
+	"Bangu", "Forta", "Cea", "Vit", "Bah", "Ata", "Figuei", "Ava", "Crici", "Cuia", "Corin"]
+
+const clubNamesRightConsonant = ["mengo", "minense", "tos", "meiras", "zeiro", "fogo", "lona", "ca", 
+	"coense", "rol", "pool", "sea", "ham", "be", "entude", "rá", "leza", "ia", "ória", "nacional", "mio",
+	"lanta", "rense", "bá"]
+
+const clubNamesRightVowel = ["inthians", "entude", "ória", "úma", "í", "asco", "afogo"]
+
+const clubNamesComposite = [" United", " Madrid", " City", " de Munique", " Casablanca", " Juniors", " Plate",
+	" Lorenzo", " Paulo", "-MG", "-PR", "-RJ", "-RN", "-GO", " Dortmund", " Zagreb", " Zvezda", " de Madrid"]
+
+const corruptionsGeneric = ["Merda", "Cocô", "Lixo", "Bosta", "Putrefa", "Desgraça", "Caralho", "Porra", "Verme",
+	"Discórdia", "Bumbum", "Otário", "Debilóide", "Rouba", "VAR", "Caga", "Mijo", "Xixi", "Urina", "Fezes", "Mia Khalifa",
+	"Gorfo", "Cachaça", "Cu"]
+
+const corruptionsLeft = ["VAR", "Putref", "Desgraç", "Caralh", "Mij", "Cag", "Roub", "Verm", "Porr", "Bost", "Coc", "Merd" ]
+
+const corruptionsRight = ["bumbumguloso", "falência"]
+
+const generateName = () => {
+	var corruptedName = "", seed_0 = generateRandomNumber(), seed_1 = generateRandomNumber();
+	switch (chooseMethod())
+	{
+		case 0:	// clubNamesLeft + corruptionsGeneric
+			corruptedName = getRandomElement(clubNamesLeft, seed_0) + getRandomElement(corruptionsGeneric, seed_1);
+		break;
+		case 1: // clubNamesLeft + corruptionsRight
+			corruptedName = getRandomElement(clubNamesLeft, seed_0) + getRandomElement(corruptionsRight, seed_1);
+		break;
+		case 2: // corruptionsGeneric + clubNamesRightConsonant
+			corruptedName = getRandomElement(corruptionsGeneric, seed_0) + getRandomElement(clubNamesRightConsonant, seed_1);
+		break;
+		case 3: // corruptionsLeft + clubNamesRightVowel
+			corruptedName = getRandomElement(corruptionsLeft, seed_0) + getRandomElement(clubNamesRightVowel, seed_1);
+		break;
+		case 4: // corruptionsGeneric + clubNamesComposite
+			corruptedName = getRandomElement(corruptionsGeneric, seed_0) + getRandomElement(clubNamesComposite, seed_1);
+		break;
+	}
+	document.getElementById("result").value = corruptedName;
+};
+
+const generateRandomNumber = () => {
+	return Math.floor((Math.random() * maxNames)) + 1;
+};
+
+const getRandomElement = (arr, number) => {
+	return arr[number % arr.length];
+};
+
+const chooseMethod = () => {
+	return Math.floor((Math.random() * (noMethods + 1)));
+}
